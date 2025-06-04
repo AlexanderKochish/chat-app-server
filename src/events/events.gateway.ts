886833b18464b700909077ca88c6616678918f17
Alexahder Kochish <shvepsolek@gmail.com>
2025-06-04 15:12:53 +0100

    fix: websocket origin

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -17,7 +17,7 @@ import { RedisService } from 'src/redis/redis.service';
 
 @WebSocketGateway({
   cors: {
-    origin: 'http://localhost:5173',
+    origin: [process.env.CLIENT_URL, 'http://localhost:5173'],
     credentials: true,
   },
 })
